4e4d2a4049f3f8c1ee4744194090ffd1d4566b68
Fernando_Sacerdote <fernando.saerdote@gmail.com>
2023-04-05 17:07:28 -0300

    cartas virando com o clique

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ function jogoNovo(){
     nCartas = prompt("Quantas cartas? (numero par entre 4 - 14)");
     const container = document.querySelector(".container");
     for (let i = 0; i < nCartas; i++){
-        container.innerHTML += '<div class="cartas"><img src="imagens/back.png" alt=""/></div>'
+        container.innerHTML += '<div class="card" onclick="selecionaCarta(this)"><div class="front-face face"><img src="imagens/back.png" alt=""/></div><div class="back-face face">Verso</div></div>'
     }
+}
+function selecionaCarta(carta){
+    carta.classList.add("selecionada");
 }
