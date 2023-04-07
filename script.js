6fb29c89a284dfbe16b27b5a254cfe111969ad7a
Fernando_Sacerdote <fernando.saerdote@gmail.com>
2023-04-07 00:56:07 -0300

    Distribuição de cartas funionando (cartas aleatórias)

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
-let nCartas;
+let nCartas = 0;
+const imgs = ["bobrossparrot.gif","explodyparrot.gif","fiestaparrot.gif","metalparrot.gif","revertitparrot.gif","tripletsparrot.gif","unicornparrot.gif"];
 jogoNovo();
 
 function jogoNovo(){
-    nCartas = prompt("Quantas cartas? (numero par entre 4 - 14)");
+    while(nCartas > 14 || nCartas < 4 || nCartas%2 === 1 ){
+        nCartas = prompt("Quantas cartas? (numero par entre 4 - 14)");
+    }
+    imgs.sort(comparador);
+    let listaImagens = [];
+    for (let i = 0; i < (nCartas/2); i++) {
+        listaImagens.push(imgs[i]);
+        listaImagens.push(imgs[i]);          
+    }
+    listaImagens.sort(comparador); 
     const container = document.querySelector(".container");
     for (let i = 0; i < nCartas; i++){
-        container.innerHTML += '<div class="card" onclick="selecionaCarta(this)"><div class="front-face face"><img src="imagens/back.png" alt=""/></div><div class="back-face face">Verso</div></div>'
+        container.innerHTML += `
+        <div class="card" onclick="selecionaCarta(this)">
+            <div class="front-face face">
+                <img src="imagens/back.png" alt=""/>
+            </div>
+            <div class="back-face face">
+                <img src="imagens/${listaImagens[i]}" alt=""/>
+            </div>
+        </div>`
     }
 }
 function selecionaCarta(carta){
     carta.classList.add("selecionada");
+}
+//----//
+function comparador() { 
+	return Math.random() - 0.5; 
 }
