a2f7395f66dfd19942981b85742a48b83a9cfe56
Fernando_Sacerdote <fernando.saerdote@gmail.com>
2023-04-07 02:25:40 -0300

    cartas iguais ficando e cartas diferentes virando

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let nCartas = 0;
+let cartasViradas = 0;
+let cartasSelecionadas = 0;
+let listaImagens = [];
 const imgs = ["bobrossparrot.gif","explodyparrot.gif","fiestaparrot.gif","metalparrot.gif","revertitparrot.gif","tripletsparrot.gif","unicornparrot.gif"];
 jogoNovo();
 
@@ -7,7 +10,7 @@ function jogoNovo(){
         nCartas = prompt("Quantas cartas? (numero par entre 4 - 14)");
     }
     imgs.sort(comparador);
-    let listaImagens = [];
+    listaImagens = [];
     for (let i = 0; i < (nCartas/2); i++) {
         listaImagens.push(imgs[i]);
         listaImagens.push(imgs[i]);          
@@ -16,8 +19,8 @@ function jogoNovo(){
     const container = document.querySelector(".container");
     for (let i = 0; i < nCartas; i++){
         container.innerHTML += `
-        <div class="card" onclick="selecionaCarta(this)">
-            <div class="front-face face">
+        <div id="${i}" class="card" onclick="selecionaCarta(this)">
+            <div class="front-face face" >
                 <img src="imagens/back.png" alt=""/>
             </div>
             <div class="back-face face">
@@ -27,7 +30,39 @@ function jogoNovo(){
     }
 }
 function selecionaCarta(carta){
+    if(carta.classList.contains("selecionada"))
+    {
+        return;
+    }
+    cartasSelecionadas++;
+    if (cartasSelecionadas > 2){
+        return;
+    }
     carta.classList.add("selecionada");
+    carta.classList.add("virada");
+    if(cartasSelecionadas === 2){
+        const viradas = document.querySelectorAll(".selecionada");
+        let id1 = viradas[0].id;
+        let id2 = viradas[1].id;
+        if (listaImagens[id1] === listaImagens[id2]) {
+            for(let i = 0; i < 2; i++){
+                viradas[i].classList.remove("selecionada");
+            }
+            cartasSelecionadas = 0;
+        }else{
+            setTimeout(desvira,1000);
+        }
+        
+    }
+}
+function desvira(){
+    const viradas = document.querySelectorAll(".selecionada");
+    cartasSelecionadas = 0;
+    for(let i = 0; i < 2; i++){
+        viradas[i].classList.remove("selecionada");
+        viradas[i].classList.remove("virada");
+    }
+    
 }
 //----//
 function comparador() { 
